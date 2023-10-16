2c1baafbe7d1b6acb6165887c5992aeea0d6af18
AliiHussein <aliunali@yahoo.com>
2023-10-16 10:40:33 +0300

    typedef structs

--- Data_types/main.c
+++ Data_types/main.c
@@ -16,14 +16,22 @@ typedef struct{
 }student;
 
 void main(){
-    student st1 = {"Ali", 20, 5.5};
+    student st1;
+    student * p1 = &st1;
+
     student st2;
+    student * p2 = &st2;
 
-    gets(st2.name);
-    scanf("%d", &st2.id);
-    scanf("%f", &st2.degree);
+    strcpy(p1->name,"Ali");
+    p1->id = 20;
+    p1->degree = 5.5;
 
-    printf("St1: Name= %s, id= %d, degree= %f\n", st1.name, st1.id, st1.degree);
-    printf("St2: Name= %s, id= %d, degree= %f\n", st2.name, st2.id, st2.degree);
+    gets(p2->name);
+    scanf("%d", &(p2->id));
+    scanf("%f", &(p2->degree));
+
+    printf("St1: Name= %s, id= %d, degree= %f\n", p1->name, p1->id, p1->degree);
+    printf("St2: Name= %s, id= %d, degree= %f\n", p2->name, p2->id, p2->degree);
+
 
 }
