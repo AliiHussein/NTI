a2a694db4d1ddc1541e481fe0a21ae6a0819de08
AliiHussein <aliunali@yahoo.com>
2023-10-16 14:33:11 +0300

    std_types and math_bit headers

--- Data_types/main.c
+++ Data_types/main.c
@@ -1,16 +1,9 @@
 #include "std_h.h"
 
 void main(){
-    Employee e1;
-    Employee e2;
-    Employee e3;
+    enum months month = jan;
 
-    Employee *ptr[3] = {&e1, &e2, &e3}; // array of pointers
-
-
-    scan_employee(ptr, 3);
-
-    print_employee(ptr, 3);
-
-
+    for(int i = jan; i <= dec; i++){
+        printf("%d ", i);
+    }
 }
--- Data_types/std_h.h
+++ Data_types/std_h.h
@@ -1,4 +1,9 @@
+#ifndef STD_H_H
+#define	STD_H_H
+
 #include <stdio.h>
+#include "std_types.h"
+#include "Bit_math.h"
 
 typedef struct{
     char name[20];
@@ -7,5 +12,9 @@ typedef struct{
 
 }Employee;
 
+enum months{jan=1, feb, mar, april, may, jun, july, aug, sep, oct, nov, dec};
+
 void print_employee(Employee * e, int n);
 void scan_employee(Employee * e, int n);
+
+#endif	/* STD_H_H */
