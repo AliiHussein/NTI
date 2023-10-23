3f06248afc721e629b38bd010d19d66348685885
AliiHussein <aliunali@yahoo.com>
2023-10-23 12:31:23 +0300

    ADC driver implementation

---
+++ Interfacing/Application/Application/MCAL/ADC/ADC.c
@@ -0,0 +1,8 @@
+/*
+ * ADC.c
+ *
+ * Created: 10/23/2023 12:11:25 PM
+ *  Author: Ali
+ */ 
+
+#include "ADC_Interface.h"
---
+++ Interfacing/Application/Application/MCAL/ADC/ADC_Interface.h
@@ -0,0 +1,17 @@
+/*
+ * ADC_Interface.h
+ *
+ * Created: 10/23/2023 12:16:30 PM
+ *  Author: Ali
+ */ 
+
+
+#ifndef ADC_INTERFACE_H_
+#define ADC_INTERFACE_H_
+
+#include "ADC_CFG.h"
+#include "ADC_Private.h"
+
+
+
+#endif /* ADC_INTERFACE_H_ */
