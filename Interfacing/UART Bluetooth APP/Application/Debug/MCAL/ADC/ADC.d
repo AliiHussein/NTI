0856fb4600f0423283e97c4e2f3b8fc911f3ce6d
AliiHussein <aliunali@yahoo.com>
2023-10-31 10:08:52 +0200

    UART bluetooth app

---
+++ Interfacing/UART Bluetooth APP/Application/Debug/MCAL/ADC/ADC.d
@@ -0,0 +1,13 @@
+MCAL/ADC/ADC.d MCAL/ADC/ADC.o: ../MCAL/ADC/ADC.c ../Bit_math.h \
+ ../std_types.h ../MCAL/ADC/ADC_Interface.h ../MCAL/ADC/ADC_Private.h \
+ ../MCAL/ADC/ADC_CFG.h
+
+../Bit_math.h:
+
+../std_types.h:
+
+../MCAL/ADC/ADC_Interface.h:
+
+../MCAL/ADC/ADC_Private.h:
+
+../MCAL/ADC/ADC_CFG.h:
