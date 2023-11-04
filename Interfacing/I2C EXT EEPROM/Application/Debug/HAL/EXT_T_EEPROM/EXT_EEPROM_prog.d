c20292691b69275681991bbecbc39a9ab91c3a73
AliiHussein <aliunali@yahoo.com>
2023-11-04 15:14:34 +0200

    EXT EEPROM APP

---
+++ Interfacing/I2C EXT EEPROM/Application/Debug/HAL/EXT_T_EEPROM/EXT_EEPROM_prog.d
@@ -0,0 +1,37 @@
+HAL/EXT_T_EEPROM/EXT_EEPROM_prog.d HAL/EXT_T_EEPROM/EXT_EEPROM_prog.o: \
+ ../HAL/EXT_T_EEPROM/EXT_EEPROM_prog.c ../std_macros.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../Bit_math.h ../std_types.h ../memMap.h ../MCAL/TWI/TWI_int.h \
+ ../HAL/EXT_T_EEPROM/EXT_EEPROM_int.h \
+ ../HAL/EXT_T_EEPROM/EXT_EEPROM_private.h
+
+../std_macros.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../Bit_math.h:
+
+../std_types.h:
+
+../memMap.h:
+
+../MCAL/TWI/TWI_int.h:
+
+../HAL/EXT_T_EEPROM/EXT_EEPROM_int.h:
+
+../HAL/EXT_T_EEPROM/EXT_EEPROM_private.h:
---
+++ Interfacing/I2C EXT EEPROM/Application/Debug/MCAL/I2C/I2C.d
@@ -0,0 +1,13 @@
+MCAL/I2C/I2C.d MCAL/I2C/I2C.o: ../MCAL/I2C/I2C.c ../std_types.h \
+ ../Bit_math.h ../MCAL/I2C/I2C_CFG.h ../MCAL/I2C/I2C_Interface.h \
+ ../MCAL/I2C/I2C_Private.h
+
+../std_types.h:
+
+../Bit_math.h:
+
+../MCAL/I2C/I2C_CFG.h:
+
+../MCAL/I2C/I2C_Interface.h:
+
+../MCAL/I2C/I2C_Private.h:
